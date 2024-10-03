65608bd7e1e190c3e1028f7846a645d46b6309e6
ViniciusSotana <vinysotana@gmail.com>
2024-10-02 21:38:36 -0300

    criacao model livros

---
+++ Models/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BibliotecaWeb.Models;
+
+namespace BibliotecaWeb.Models
+{
+    [Table("Livro")]
+    public class Livro
+    {
+        [Key]
+        [Display(Name ="Livro: ")]
+        public int livroId { get; set; }
+
+        [Display(Name = "Titulo: ")]
+        [Required(ErrorMessage = "Campo Titulo é obrigatório")]
+        public string titulo { get; set; }
+
+        [Display(Name = "Autor: ")]
+        public int autorId { get; set; }
+
+        [Display(Name = "Autor: ")]
+        public Autor autor { get; set; }
+
+
+
+        [Display(Name = "Genero: ")]
+        public int generoId { get; set; }
+
+        [Display(Name = "Genero: ")]
+        public Genero genero { get; set; }
+
+
+
+        [Display(Name = "Ano de Publicacao: ")]
+        [Required(ErrorMessage = "Campo ano de publicação é obrigatório")]
+        public int publicacao { get; set; }
+
+        [Display(Name = "Quantia em Estoque: ")]
+        public int quantiaEstoque { get; set; }
+    }
+}
